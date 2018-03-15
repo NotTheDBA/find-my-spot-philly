65c946e57bbb83cdbe23e454d8ebaaac3f7ae383
Randy Reitz <rsreitz@outlook.com>
2018-03-14 20:08:25 -0400

    Disabled javascript not ready for primetime.

--- logic.js
+++ logic.js
@@ -23,28 +23,28 @@ var employeeRate = 0;
 // This function allows you to update your page in real-time when the firebase database changes.
 database.ref().on("value", function(snapshot) {
 
-        // Change this to find the most recent added record
-        if (snapshot.child("highemployee").exists() && snapshot.child("highrate").exists()) {
+        //     // Change this to find the most recent added record
+        //     if (snapshot.child("highemployee").exists() && snapshot.child("highrate").exists()) {
 
-            // Set the variables for highemployee/highrate equal to the stored values in firebase.
+        //         // Set the variables for highemployee/highrate equal to the stored values in firebase.
 
-            // Change the HTML to reflect the stored values
-            $("#recent-employee").text(highemployee);
-
-
-        }
-
-        // Else Firebase doesn't have a highrate/highemployee, so use the initial local values.
-        else {
-
-            // Change the HTML to reflect the initial values
-            $("#recent-employee").text(initialBid);
-            $("#recent-rate").text(initialemployee);
-
-        }
+        //         // Change the HTML to reflect the stored values
+        //         $("#recent-employee").text(highemployee);
 
 
-        // If any errors are experienced, log them to console.
+        //     }
+
+        //     // Else Firebase doesn't have a highrate/highemployee, so use the initial local values.
+        //     else {
+
+        //         // Change the HTML to reflect the initial values
+        //         $("#recent-employee").text(initialBid);
+        //         $("#recent-rate").text(initialemployee);
+
+        //     }
+
+
+        //     // If any errors are experienced, log them to console.
     },
     function(errorObject) {
         console.log("The read failed: " + errorObject.code);
@@ -53,35 +53,35 @@ database.ref().on("value", function(snapshot) {
 // --------------------------------------------------------------
 
 // Whenever a user clicks the submit-bid button
-$("#submit-bid").on("click", function(event) {
+$("#add-employee").on("click", function(event) {
     // Prevent form from submitting
     event.preventDefault();
 
-    // Get the input values
-    employeerate = parseInt($("#employee-rate").val());
-    employeeName = $("#employee-name").val();
+    // // Get the input values
+    // employeeRate = parseInt($("#employee-rate").val());
+    // employeeName = $("#employee-name").val();
 
 
-    if (employeerate > highrate) {
+    // if (employeeRate > highrate) {
 
-        // Save the new rate in Firebase
-        database.ref().set({
-            highrate: employeerate,
-            highemployee: employeeName
-        })
+    //     // Save the new rate in Firebase
+    //     database.ref().set({
+    //         highrate: employeeRate,
+    //         highemployee: employeeName
+    //     })
 
-        // Store the new high rate and employee name as a local variable
-        highrate = employeerate
-        highemployee = employeeName
+    //     // Store the new high rate and employee name as a local variable
+    //     highrate = employeeRate
+    //     highemployee = employeeName
 
 
-        // Change the HTML to reflect the new high rate and employee
-        $("#recent-employee").text(highemployee);
-        $("#recent-rate").text(highrate);
+    //     // Change the HTML to reflect the new high rate and employee
+    //     $("#recent-employee").text(highemployee);
+    //     $("#recent-rate").text(highrate);
 
-    } else {
-        // Alert
-        alert("Sorry that bid is too low. Try again.");
-    }
+    // } else {
+    //     // Alert
+    //     alert("Sorry that bid is too low. Try again.");
+    // }
 
 });
