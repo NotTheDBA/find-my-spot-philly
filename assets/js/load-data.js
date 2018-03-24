3ae9c8445d67a8ef4a14f27094dae205b9b5f9fd
Randy Reitz <rsreitz@outlook.com>
2018-03-24 11:24:58 -0400

    Load our reference data in to Firebase.

--- assets/js/load-data.js
+++ assets/js/load-data.js
@@ -1,15 +1,27 @@
+// Initialize Firebase
+var config = {
+    apiKey: "***REMOVED***",
+    authDomain: "find-my-spot-philly.firebaseapp.com",
+    databaseURL: "https://find-my-spot-philly.firebaseio.com",
+    projectId: "find-my-spot-philly",
+    storageBucket: "",
+    messagingSenderId: "***REMOVED***"
+};
+
+firebase.initializeApp(config);
+// Create a variable to reference the database
+var database = firebase.database();
+
+
 $(document).ready(function() {
-    //this initializes a global object called "Hoods"
-    //It contains just a list of neighborhood names
-    // use:
-    // Hoods.List[0]  - gets the name of neighborhood
-    getList();
+
 
     //this initializes a second global object called "Philly"
     // use:
     // Philly[10] - Get all neighborhood data
     // Philly[10].Name - gets the name of neighborhood
     getData();
+
 
     //this initializes a third global object called "MapData"
     // use:
@@ -35,35 +47,55 @@ function makeLoadButton(label) {
     var button = $("<button>").text(label).addClass("btn btn-primary btn-block");
     button.on("click", function() {
 
-        console.log(Object.keys(Hoods.List).length);
-        // console.log(Hoods);
-        // console.log(Hoods.List); 
-        // console.log(Hoods.List[10]);
 
-        console.log(Object.keys(Philly).length);
-        // console.log(Philly);
-        // console.log(Philly[10]);
-        // console.log(Philly[10].Neighborhood);
-
-        console.log(Object.keys(MapData.Map).length);
-        // console.log(MapData);
-        // console.log(MapData.Map);
-        // console.log(MapData.Map[0]);
-        // console.log(MapData.Map[0].properties);
-        // console.log(MapData.Map[0].properties.listname);
-
-        console.log(MapData.Map[0].geometry.coordinates);
+        loadList();
+        loadIncomes();
+        // pushIncomes();
+        verifyCounts();
     });
 
     $("#loader").empty().append(button);
 }
 
-function getcount() {
-    return Object.keys(MapData.Neighborhoods.Map).length;
+function verifyCounts() {
+    // return Object.keys(MapData.Neighborhoods.Map).length;
+
+    // console.log(Object.keys(Hoods.List).length);
+    // console.log(Hoods);
+    // console.log(Hoods.List); 
+    // console.log(Hoods.List[10]);
+
+    console.log(Object.keys(Philly).length);
+    // console.log(Philly);
+    // console.log(Philly[10]);
+    // console.log(Philly[10].Name);
+    // console.log(Philly[10]["Median household income in 2016"]);
+    // console.log(Philly[10]["Median household income in 2016"][Philly[10].Name]);
+
+    console.log(Object.keys(MapData.Map).length);
+    // console.log(MapData);
+    // console.log(MapData.Map);
+    // console.log(MapData.Map[0]);
+    // console.log(MapData.Map[0].properties);
+    // console.log(MapData.Map[0].properties.listname);
+
+    // console.log(MapData.Map[0].geometry.coordinates);
 }
 
+function pushIncomes() {
 
-function getList() {
+    console.log(Philly[10].Name);
+    // console.log(Philly[10]["Median household income in 2016"]);
+    console.log(Philly[10]["Median household income in 2016"][Philly[10].Name]);
+
+}
+
+function loadList() {
+    //this initializes a global object called "Hoods"
+    //It contains just a list of neighborhood names
+    // use:
+    // Hoods.List[0]  - gets the name of neighborhood
+
     // Only needs to run once on load.
     var queryurl = "assets/data/neighborhoods-list.json";
     $.ajax({
@@ -74,7 +106,54 @@ function getList() {
         //puts the data in our global space
         window.Hoods = jsonData;
         // console.log(jsonData);
+        // database.ref("Hoods").child("List").push(Hoods.List)
+        database.ref("Hoods").child("List").remove()
+        database.ref("Hoods").child("List").set(Hoods.List)
     });
+
+}
+
+
+function loadIncomes() {
+
+    var queryurl = "assets/data/n1.json";
+    $.ajax({
+        url: queryurl,
+        dataType: 'json',
+        method: "GET"
+    }).then(function(jsonData) {
+        //puts the data in our global space
+        window.Philly = jsonData;
+
+        database.ref("Philly").remove()
+        var hoodsRef = database.ref("Philly").child("hoods");
+
+        var hoodCount = 0;
+        Philly.forEach(thisHood => {
+            if (typeof thisHood["Median household income in 2016"] !== 'undefined') {
+                hoodsRef.child(hoodCount).child("name").set(thisHood.Name)
+                hoodsRef.child(hoodCount).child("median-income").set(thisHood["Median household income in 2016"][thisHood.Name])
+                hoodCount += 1;
+            }
+
+
+            // hoodsRef.child(hoodCount).child("name").set(thisHood.Name)
+            // 
+        });
+
+
+        // hoodsRef.child(10).child("name").set(thisHood.Name)
+        // hoodsRef.child(10).child("median-income").set(thisHood["Median household income in 2016"][thisHood.Name])
+
+        // database.ref("Philly").child("hoods").child(10).child("name").set(Philly[10].Name)
+        // database.ref("Philly").child("hoods").child(10).child("median-income").set(Philly[10]["Median household income in 2016"][Philly[10].Name])
+
+        // console.log(Philly[10].Name);
+        // // console.log(Philly[10]["Median household income in 2016"]);
+        // console.log(Philly[10]["Median household income in 2016"][Philly[10].Name]);
+        // console.log(jsonData);
+    });
+
 
 }
 
